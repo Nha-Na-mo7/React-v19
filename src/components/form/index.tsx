bca12ef03d0b5db6c8d49cef05f3a9e8322e9ca4
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-12-16 18:18:37 +0900

    feat: add map prefectures option

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { Prefectures } from "../../const/Prefectures";
 
 export const Form: FC = (props) => {
   return (
@@ -32,9 +33,14 @@ export const Form: FC = (props) => {
             <td>
               <select name="prefecture">
                 <option hidden>都道府県を選択してください</option>
-                <option value="1">北海道</option>
-                <option value="13">東京都</option>
-                <option value="47">沖縄県</option>
+                {Prefectures.map((prefecture) => {
+                  const { prefectureCode, prefectureName } = prefecture;
+                  return (
+                    <option key={prefectureName} value={prefectureCode}>
+                      {prefectureName}
+                    </option>
+                  );
+                })}
               </select>
             </td>
           </tr>
