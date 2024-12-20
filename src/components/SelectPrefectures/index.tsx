100025e86a20ca732b062014a842af1ea791cceb
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-12-20 22:49:13 +0900

    feat: create SelectPrefectures component

---
+++ src/components/SelectPrefectures/index.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { Prefectures } from "../../const/Prefectures";
+
+type Props = {
+  selectedPrefectureCode: `${number}`;
+};
+
+export const SelectPrefectures: FC<Props> = ({ selectedPrefectureCode }) => {
+  return (
+    <select name="prefecture" defaultValue={selectedPrefectureCode}>
+      <option hidden>都道府県を選択してください</option>
+      {Prefectures.map((prefecture) => {
+        const { prefectureCode, prefectureName } = prefecture;
+        return (
+          <option key={prefectureName} value={prefectureCode}>
+            {prefectureName}
+          </option>
+        );
+      })}
+    </select>
+  );
+};
