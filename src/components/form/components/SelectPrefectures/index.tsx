2ba6838c649c6027d9272e04e0a14ca3ee058ed0
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-12-31 21:25:45 +0900

    feat: add getCurrentPrefectureCode function in currentPrefectureCode

--- src/components/form/components/SelectPrefectures/index.tsx
+++ src/components/form/components/SelectPrefectures/index.tsx
@@ -1,18 +1,35 @@
 import { FC } from "react";
 import { Prefectures } from "../../../../const/Prefectures";
+import { Prefecture } from "../../../../types";
 
 type Props = {
-  selectedPrefectureCode: `${number}`;
+  selectedPrefectureCode: string;
+};
+
+const getCurrentPrefectureCode = (
+  prefectures: Prefecture[],
+  selectedId: string
+): string => {
+  const selected = prefectures.find(
+    (prefecture) => prefecture.prefectureCode === selectedId
+  )?.prefectureName;
+  return selected ?? "";
 };
 
 export const SelectPrefectures: FC<Props> = ({ selectedPrefectureCode }) => {
   return (
-    <select name="prefecture" defaultValue={selectedPrefectureCode}>
-      <option hidden>都道府県を選択してください</option>
+    <select
+      name="prefecture"
+      defaultValue={getCurrentPrefectureCode(
+        [...Prefectures],
+        selectedPrefectureCode
+      )}
+    >
+      <option value={""}>都道府県を選択してください</option>
       {Prefectures.map((prefecture) => {
         const { prefectureCode, prefectureName } = prefecture;
         return (
-          <option key={prefectureName} value={prefectureCode}>
+          <option value={prefectureName} key={prefectureCode}>
             {prefectureName}
           </option>
         );
