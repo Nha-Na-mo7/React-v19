9656092c892281d76584597729347461165a1930
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-12-31 11:36:21 +0900

    feat: React 19, not use forwardRef

--- src/components/form/components/Input/index.tsx
+++ src/components/form/components/Input/index.tsx
@@ -1,14 +1,12 @@
-import React, { CSSProperties, ComponentPropsWithRef } from "react";
+import { CSSProperties, ComponentPropsWithRef, FC } from "react";
+
 const inputStyle: CSSProperties = {
   border: "solid 1px black",
   borderRadius: "3px",
   padding: "2px 4px",
 };
 
-/* React19でforwardRef関連記述が不要に */
-export const Input = React.forwardRef<
-  HTMLInputElement,
-  ComponentPropsWithRef<"input">
->((props, reference) => (
-  <input {...props} ref={reference} style={inputStyle} />
-));
+/** React19: forwardRefを挟まず、refを取得可能 */
+export const Input: FC<ComponentPropsWithRef<"input">> = (props) => {
+  return <input {...props} ref={props.ref} style={inputStyle} />;
+};
