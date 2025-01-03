725c9352c0d9d55bd195900d6e9c7f07e3c42085
Nha-Na-mo7 <zirconiump42@gmail.com>
2025-01-03 21:30:20 +0900

    feat: Form container component

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,41 +1,13 @@
 import { FC } from "react";
-import { SelectPrefectures } from "./components/SelectPrefectures";
-import { ErrorMessage } from "./components/ErrorMessage";
-import { PostalCodeInputs } from "./components/PostalCodeInputs";
+import { useSearchAddress } from "./hooks/useSearchAddress";
+import { FormTemplate } from "./template";
 
-// dummy const
-const selectedPrefectureCode = "0";
-const isError = false;
-const errorMessage = "色々エラーになりました";
+/**
+ * @remarks
+ * container component
+ */
+export const Form: FC = () => {
+  const dependencies = useSearchAddress();
 
-export const Form: FC = (props) => {
-  return (
-    <form name="address" style={{}}>
-      <table>
-        <tbody>
-          {isError && <ErrorMessage>{errorMessage}</ErrorMessage>}
-          <tr>
-            <th>郵便番号</th>
-            <td>
-              <PostalCodeInputs onChange={() => {}} />
-            </td>
-          </tr>
-          <tr>
-            <th>都道府県名</th>
-            <td>
-              <SelectPrefectures
-                selectedPrefectureCode={selectedPrefectureCode}
-              />
-            </td>
-          </tr>
-          <tr>
-            <th>市区町村名</th>
-            <td>
-              <input style={{ width: "100%" }} name="citiesInput" />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </form>
-  );
+  return <FormTemplate {...dependencies} />;
 };
